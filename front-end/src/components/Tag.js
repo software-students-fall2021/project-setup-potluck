57f36edd608a19dcf9d3e8a541d3d5ffb6e76f41
seungguini <sl6369@nyu.edu>
2021-10-24 14:38:27 -0400

    Add color changes to tags when selected/deselected

--- front-end/src/components/Tag.js
+++ front-end/src/components/Tag.js
@@ -4,14 +4,24 @@ import "../styles/Tag.css"
 
 const Tag = (props) => {
   const [tagSelected, setTagSelected] = useState(true)
+  const [color, setColor] = useState("#0096c7")
 
   useEffect(() => {
     let resultstr = tagSelected ? "selected" : "not selected"
     console.log(`${props.name} is ${resultstr}`)
   }, [tagSelected, props.name])
 
+  const clicked = () => {
+    setTagSelected(!tagSelected)
+    setColor(tagSelected ? "#caf0f8" : "#0096c7")
+  }
+
   return (
-    <div className="tag" onClick={() => setTagSelected(!tagSelected)}>
+    <div
+      className="tag"
+      onClick={() => clicked()}
+      style={{ "background-color": color }}
+    >
       <p1>{props.name}</p1>
     </div>
   )
