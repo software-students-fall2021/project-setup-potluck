5be8f9cbaa136039fff2c7cd63ceffc75f293290
Lauren Gatesman <55667325+lkgatesman@users.noreply.github.com>
2021-10-27 22:41:50 -0400

    Update main pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,8 +3,6 @@ import "./App.css"
 // Import components
 import TagButton from "./components/TagButton"
 
-import SampleComponent from "./components/SampleComponent"
-
 function App() {
   return (
     <div>
