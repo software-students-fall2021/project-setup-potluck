2377836d899a801b49c2db6d5686d4d1c7853f1f
seungguini <sl6369@nyu.edu>
2021-10-28 22:08:49 -0400

    Put Header, Tag Button, and Map together

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,27 +14,14 @@ import {
   Button,
 } from "react-bootstrap"
 import Header from "./components/Header.js"
+import Map from "./components/Map.js"
 
 function App() {
   return (
     <div>
       <Header />
-      {/*
       <TagButton className="tagButton" />
-      <h1>Example Text</h1>
-      <p1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec nulla
-        placerat, viverra metus a, faucibus magna. Pellentesque feugiat erat in
-        eros finibus, non feugiat massa eleifend. Mauris ut leo lorem. Praesent
-        vestibulum porta mi, ac gravida ante mollis nec. Morbi et venenatis
-        odio, eget accumsan augue. Aenean vitae quam a diam interdum euismod. Ut
-        tincidunt varius neque, at fermentum diam molestie sit amet. In vel est
-        orci. Integer viverra ut quam quis vestibulum. Nunc in maximus leo, a
-        venenatis mi. Vivamus mollis faucibus auctor. Quisque iaculis placerat
-        dictum.
-      </p1> */}
-      {/* Uncomment postpage to see the post page route for now */}
-      {/* <PostPage/> */}
+      <Map />
     </div>
   )
 }
