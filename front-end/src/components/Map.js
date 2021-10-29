fe911802b1c8998d3cb2c9a8c84944c90492bf50
seungguini <sl6369@nyu.edu>
2021-10-28 23:53:47 -0400

    Add randomized points to the map

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,15 +1,30 @@
-import React from "react"
-import ReactMapGl from "react-map-gl"
+import React, { useCallback, useEffect, useState } from "react"
+import ReactMapGl, { Marker } from "react-map-gl"
 
+import Pin from "./Pin"
 // Load API key from .env file (WHICH SHOULD BE IN .gitignore)
 require("dotenv").config()
-
-// Grab API Key for Mapbox
-const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX
 
 // This component loads an interactive map using react-map-gl
 // which is a React wrapper package for deck.gl
 const Map = () => {
+  // Grab API Key for Mapbox
+  const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX
+
+  // Define default location to load map
+  const DEFAULT_LONGITUDE = -73.9936
+  const DEFAULT_LATITUDE = 40.7218
+  const DEFAULT_SPRAD = 0.02
+
+  // Temporary function to generate random longitude OR latitude, centered around
+  // a given location with a given spread
+  const getRandomLocation = (location, spread) => {
+    console.log(spread)
+    const randomLocation = spread / 2 - spread * Math.random() + location
+    console.log(randomLocation)
+    return randomLocation
+  }
+
   // viewport determines the user's view of the map
   const [viewport, setViewport] = React.useState({
     // Default values for the viewport when the App is loaded
@@ -19,6 +34,25 @@ const Map = () => {
     pitch: 0,
     bearing: 0,
   })
+
+  let tempArr = []
+
+  // Fake data filler to create an array of markers
+  const [markerArr, setMarkerArr] = useState(tempArr)
+
+  useEffect(() => {
+    tempArr = [...markerArr]
+    for (let i = 0; i < 100; i++) {
+      tempArr.push({
+        longitude: getRandomLocation(DEFAULT_LONGITUDE, DEFAULT_SPRAD),
+        latitude: getRandomLocation(DEFAULT_LATITUDE, DEFAULT_SPRAD),
+      })
+    }
+
+    setMarkerArr(tempArr)
+  }, [])
+
+  console.log(markerArr)
 
   return (
     <ReactMapGl
@@ -31,7 +65,18 @@ const Map = () => {
       onViewportChange={setViewport}
       // Provide Mapbox API key
       mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
-    />
+    >
+      {markerArr.map((restuarant) => {
+        return (
+          <Marker
+            latitude={restuarant.latitude}
+            longitude={restuarant.longitude}
+          >
+            <Pin size={15} />
+          </Marker>
+        )
+      })}
+    </ReactMapGl>
   )
 }
 
