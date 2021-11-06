caa407aacf7dc73a05d5e5ee954e54f5823d69e1
Jin Hyeong Kim <jhk742@nyu.edu>
2021-11-06 16:34:13 -0400

    elements within BiggerContainer now have PopUp options

--- front-end/src/components/BiggerContainer.js
+++ front-end/src/components/BiggerContainer.js
@@ -1,17 +1,20 @@
 import "../styles/BiggerContainer.css"
+import FoodPopUp from "./FoodPopUp.js"
+import { useState } from 'react'
 
 const BiggerContainer = () => {
+    const [buttonPopUp, setButtonPopUp] = useState(false)
     return (
         <div class="grid">
             <br></br>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
-            <div classname="mainDishSpace"><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
+            <div classname="mainDishSpace"><button onClick={() => setButtonPopUp(true)}><img src="https://picsum.photos/200/200" alt="first" className="rounded2"></img></button><FoodPopUp trigger={buttonPopUp} setTrigger={setButtonPopUp}><h1>Name_Of_Food</h1><p>space for recipe + nutrtional content</p></FoodPopUp><div className="cent">Name_of_Food</div></div>
         </div>
     )
 }
