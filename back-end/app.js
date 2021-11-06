d9da52ed9d512c72151ffb558f5d348ed5985689
seungguini <sl6369@nyu.edu>
2021-11-05 22:45:38 -0400

    Refactor code to create separate .js files for individual routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,10 @@
-// import and instantiate express
+// ---------------- IMPORT EXPRESS  ---------------- 
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 const path = require("path")
 
-// import some useful middleware
+// ---------------- IMPORT MIDDLEWARE  ---------------- 
+
 const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
 const axios = require("axios") // middleware for making requests to APIs
 require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
@@ -20,44 +21,14 @@ app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming P
 // make 'public' directory publicly readable with static content
 app.use("/static", express.static("public"))
 
-// Handle GET request to grab all restaurant information (no filter)
-app.get("/restaurants", (req, res) => {
+// ---------------- IMPORT ROUTES  ---------------- 
 
-    // ----- GENERATE RANDOM RESTAURANT LOCATIONS - to be deleted -----
-    // Define default location to load map
-    const DEFAULT_LONGITUDE = -73.9936
-    const DEFAULT_LATITUDE = 40.7218
-    const DEFAULT_SPRAD = 0.02
+const restaurantsRoute = require("./routes/restaurants");;
 
-    // Temporary function to generate random longitude OR latitude, centered around
-    // a given location with a given spread
-    const getRandomLocation = (location, spread) => {
-        return spread / 2 - spread * Math.random() + location
-    }
-    
-    // Define number of restaurants to randomly generate
-    NO_RESTAURANTS = 20
+// ---------------- ROUTES  ---------------- 
 
-    // Create random restaurants
-    restaurants = []
-    for (let i = 0; i < NO_RESTAURANTS; i++) {
-        restaurants.push({
-            "restaurant_id": i,
-            "name": "restaurant1",
-            "number": "123-456-7890",
-            "address": "123 5th Ave, New York, NY, 10003",
-            "no_posts": Math.floor(Math.random() * 100),
-            "location": {
-                "longitude": getRandomLocation(-73.9936, 0.01),
-                "latitude": getRandomLocation(-73.9936, 0.01),
-            },
-            "menu": ["menu1", "menu2", "menu3", "menu4"]
-        })
-    }
-
-    // Send restaurants as response
-    res.send(restaurants)
-})
+// Routes for querying MongoDB - users & exercises
+app.use("/restaurants", restaurantsRoute);
 
 // Export the express app
 module.exports = app
---
+++ back-end/routes/restaurants.js
@@ -0,0 +1,53 @@
+// Routes for "/restaurants"
+const router = require("express").Router(); // Router object to define a route
+
+// Temporary function to generate random longitude OR latitude, centered around
+// a given location with a given spread
+const getRandomLocation = (location, spread) => {
+    return spread / 2 - spread * Math.random() + location
+}
+
+// Function to randomly generate fake restaurant data
+// (To be replaced with proxy requests to MongoDB)
+const getRestaurants = (req, res) => {
+
+    // ----- GENERATE RANDOM RESTAURANT LOCATIONS - to be deleted -----
+    // Define default location to load map
+    const DEFAULT_LONGITUDE = -73.9936
+    const DEFAULT_LATITUDE = 40.7218
+    const DEFAULT_SPREAD = 0.01
+
+    // Define number of restaurants to randomly generate
+    NO_RESTAURANTS = 20
+
+    // Create random restaurants
+    restaurants = []
+    for (let i = 0; i < NO_RESTAURANTS; i++) {
+        restaurants.push({
+            "restaurant_id": i,
+            "name": "restaurant1",
+            "number": "123-456-7890",
+            "address": "123 5th Ave, New York, NY, 10003",
+            "no_posts": Math.floor(Math.random() * 100),
+            "location": {
+                "longitude": getRandomLocation(DEFAULT_LONGITUDE, DEFAULT_SPREAD),
+                "latitude": getRandomLocation(DEFAULT_LATITUDE, DEFAULT_SPREAD),
+            },
+            "menu": ["menu1", "menu2", "menu3", "menu4"]
+        })
+    }
+
+    // Send restaurants as response
+    res.send(restaurants)
+    
+}
+
+// ------------------ ROUTE ENDPOINTS ------------------
+
+// Define an endpoint to handle GET request to grab all restaurant information (no filter)
+// TO-DO : Implement filtered GET request
+router.route("/").get((req, res) => {
+    getRestaurants(req, res);
+});
+  
+module.exports = router
