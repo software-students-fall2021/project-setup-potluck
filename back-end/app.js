6529431fbd37464f041d38bea31d5ecd8971057c
Christian B Weinschenk <cbw307@nyu.edu>
2021-11-07 01:33:24 -0400

    GET: /Search route successfully implemented
    Get route was implemented using mockaroo API data! Route will respond with post data generate from mockaroo.

--- back-end/app.js
+++ back-end/app.js
@@ -3,14 +3,15 @@ const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 const path = require("path")
 const cors = require("cors");
+//const fetch = require("node-fetch");
 // ---------------- IMPORT MIDDLEWARE  ---------------- 
-
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
 const axios = require("axios") // middleware for making requests to APIs
 require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
 const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 const { METHODS } = require("http")
-
+const mockaroo = require("mockaroo")
 // use the morgan middleware to log all incoming http requests
 app.use(morgan("dev")) // morgan has a few logging default styles - dev is a nice concise color-coded style
 
@@ -35,10 +36,14 @@ app.use("/static", express.static("public"))
 
 const restaurantsRoute = require("./routes/restaurants")
 
+const searchRoute = require("./routes/searchRoute")
+
 // ---------------- ROUTES  ---------------- 
 
 // Route for restaurant GET requests
 app.use("/restaurants", restaurantsRoute);
+
+app.use("/search", searchRoute);
 
 // Export the express app
 module.exports = app
