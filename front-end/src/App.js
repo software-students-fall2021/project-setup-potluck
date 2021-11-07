aa7971474c00da15de2cf17225317e763e726d87
seungguini <sl6369@nyu.edu>
2021-11-07 17:56:52 -0500

    Fix restaurant link to restaurants, add RestaurantFeed component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,15 @@ import "./App.css"
 
 // Import components
 
-import Header from "./components/Header.js"
-import Map from "./components/Map.js"
+import Header from "./components/Header"
+import Map from "./components/Map"
 import Login from "./components/Login"
-import FeedPage from "./components/RestaurantPage"
+import RestaurantFeed from "./components/RestaurantFeed"
 import TagButton from "./components/TagButton"
 import InitialView from "./components/InitialView"
 import About from "./components/About"
-import Footer from "./components/Footer.js"
+import Footer from "./components/Footer"
+import RestaurantPage from "./components/RestaurantPage"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 
@@ -26,7 +27,7 @@ function App() {
             <div id="hamitems">
               <a href="/">Initial view</a>
               <a href="/feed">Feed</a>
-              <a href="/restaurant">Restaurant</a>
+              <a href="/restaurants">Restaurants</a>
               <a href="/map">Map</a>
               <a href="/about">About</a>
             </div>
@@ -37,15 +38,18 @@ function App() {
             <TagButton />
             <Header />
           </Route>
-          <Route path="/restaurant">
-            <FeedPage />
+          <Route path="/restaurants">
+            <RestaurantFeed />
           </Route>
           <Route path="/map">
             <Map />
           </Route>
           <Route path="/about">
-              <About />
+            <About />
           </Route>
+          
+          {/* Route with restaurant id passed as a parameter */}
+          <Route path="/restaurant/:id" children={<RestaurantPage />}/>
           {/* Dont add routes after the base route they wont work*/}
           <Route path="/">
     
