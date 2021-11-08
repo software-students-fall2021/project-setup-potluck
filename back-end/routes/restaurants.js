bfb0d5ea75bb5af5a8ef226494fdf5c3e7f999e2
seungguini <sl6369@nyu.edu>
2021-11-07 23:27:11 -0500

    Add comments for back-end restaurant endpoint

--- back-end/routes/restaurants.js
+++ back-end/routes/restaurants.js
@@ -11,58 +11,17 @@ const getRandomLocation = (location, spread) => {
 // Function to randomly generate fake restaurant data
 // (To be replaced with proxy requests to MongoDB)
 const getRestaurants = async (req, res) => {
+
+    // Grab array of restaurant JSONs from Mockaroo
     try {
         console.log("AXIOS RESULTS")
         const response = await axios('https://api.mockaroo.com/api/ef7d4180?count=200&key=e5beaaa0')
-        const data = response.data
+        const data = response.data // Extract data from mockaroo
 
         res.send(data)
     } catch (error) {
         res.send(error)
     }
-        
-
-    // try{
-    //     // ----- GENERATE RANDOM RESTAURANT LOCATIONS - to be deleted -----
-    //     // Define default location to load map
-    //     const DEFAULT_LONGITUDE = -73.9936
-    //     const DEFAULT_LATITUDE = 40.7218
-    //     const DEFAULT_SPREAD = 0.01
-
-    //     // Define number of restaurants to randomly generate
-    //     NO_RESTAURANTS = 20
-
-    // // Create random restaurants
-    // restaurants = []
-    // for (let i = 0; i < NO_RESTAURANTS; i++) {
-    //     restaurants.push({
-    //         "id": i,
-    //         "name": "restaurant1",
-    //         "number": "123-456-7890",
-    //         "address": "123 5th Ave, New York, NY, 10003",
-    //         "no_posts": Math.floor(Math.random() * 100),
-    //         "backgroundPic" : "https://picsum.photos/0/200", 
-    //         "location": {
-    //             "longitude": getRandomLocation(DEFAULT_LONGITUDE, DEFAULT_SPREAD),
-    //             "latitude": getRandomLocation(DEFAULT_LATITUDE, DEFAULT_SPREAD),
-    //         },
-    //         "logoPic" : "https://picsum.photos/250/250",
-    //         "menuPopular": ["menu1", "menu2", "menu3", "menu4"],
-    //         "recipesPopular": ["recipe1", "recipe2", "recipe3", "recipe4"],
-    //         "menuPopularPics": ["https://picsum.photos/200/200", "https://picsum.photos/200/200", "https://picsum.photos/200/200", "https://picsum.photos/200/200"],
-    //         "menuMain": ["main1", "main2", "main3", "main4", "main5", "main6", "main7", "main8"],
-    //         "recipesMain" : ["recipe1", "recipe2", "recipe3", "recipe4", "recipe5", "recipe6", "recipe7", "recipe8"],
-    //         "menuMainPics": ["https://picsum.photos/200/200","https://picsum.photos/200/200","https://picsum.photos/200/200","https://picsum.photos/200/200","https://picsum.photos/200/200","https://picsum.photos/200/200","https://picsum.photos/200/200","https://picsum.photos/200/200",],
-    //         "beverages": ["bev1", "bev2", "bev3", "bev4"]
-            
-    //     })
-    // }
-    //     return restaurants
-    // }
-    
-    // catch(error){
-    //     return error
-    // }
     
 }
 
