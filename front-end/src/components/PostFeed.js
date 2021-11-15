45c701ac43fc3685c379a475944be8dcfb2cb6a1
Wajahat Mirza <mwm356@nyu.edu>
2021-11-15 17:06:41 -0500

    FE of posting feed

--- front-end/src/components/PostFeed.js
+++ front-end/src/components/PostFeed.js
@@ -5,39 +5,30 @@ import "../styles/PostFeed.css"
 
 
 const PostFeed = () => {
-    return (
-        // <div class="entire-frame">
-        //     {/* <p>hello</p> */}
-        //     <form class="post-draft">
-        //         <textarea>Some text...</textarea>
-        //     </form>
-
-
-        // </div>
-    
+    return (    
         <form action method="post">
   
-            <h1><strong>File upload</strong> with style and pure CSS</h1>
+            <h1><strong>Your Post</strong>: Share Food Love with everyone</h1>
             
             <div class="form-group">
-                <label for="title">Title <span>Use title case to get a better result</span></label>
-                <input type="text" name="title" id="title" class="form-controll"/>
+                <label for="title"></label>
+                <input type="text" name="title" id="title" class="form-controll" placeholder="Title of the Post"/>
             </div>
             <div class="form-group">
-                <label for="caption">Caption <span>This caption should be descriptiv</span></label>
-                <textarea type="text" name="caption" id="caption" class="form-controll"/>
+                <label for="caption"></label>
+                <textarea type="text" name="caption" id="caption" class="form-controll" placeholder="Add more info"/>
             </div>
             
             <div class="form-group file-area">
-                    <label for="images">Images <span>Your images should be at least 400x300 wide</span></label>
+                    {/* <label for="images">Add Images</label> */}
                 <input type="file" name="images" id="images" required="required" multiple="multiple"/>
                 <div class="file-dummy">
-                <div class="success">Great, your files are selected. Keep on.</div>
+                <div class="success">Click here to upload</div>
                 {/* <div class="default">Please select some files</div> */}
                 </div>
             </div>
             
-            <div class="form-group">
+            <div class="form-group2">
                 <button type="submit">Post</button>
             </div>
             
@@ -48,8 +39,6 @@ const PostFeed = () => {
     
     
         )
-    
-
 
 }
 
