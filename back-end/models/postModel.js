aeb3433c011a27b914764f55a07e3cad70e5668d
HaveACookie <36010328+HaveACookie@users.noreply.github.com>
2021-11-21 14:12:28 -0500

    Removed uneccesary array in schema

--- back-end/models/postModel.js
+++ back-end/models/postModel.js
@@ -7,7 +7,7 @@ const URLSlugs = require('mongoose-url-slugs');
 const Post = new mongoose.Schema({
     //not sure if unique needs to be here
     title:{type: String, unique: true},
-    author:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
+    author:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
     content:{type: String},
     parentRestaurant: [{type:mongoose.Schema.Types.ObjectId, ref:'Restuarant'}],
     imgs:[{type: String}]
