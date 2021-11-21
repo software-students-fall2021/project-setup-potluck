4f19c5fba17520fb7c3c399ae122ebc5d69f5b0a
Christian B Weinschenk <cbw307@nyu.edu>
2021-11-21 15:22:40 -0500

    Added additional elements in Post schema
    Pretty self explanatory some minor corrections.

--- back-end/models/postModel.js
+++ back-end/models/postModel.js
@@ -10,8 +10,28 @@ const Post = new mongoose.Schema({
     author:[{type:mongoose.Schema.Types.ObjectId, ref:'User'}],
     content:{type: String},
     parentRestaurant: [{type:mongoose.Schema.Types.ObjectId, ref:'Restuarant'}],
-    imgs:[{type: String}]
+    url:{type: String},
+    //subject to change to an image url
+    imgs:[{type: String}],
+    tags:[{type : String}]
 })
+
+// const postSchema = {
+//     //schema configuration
+//     title: "postSchema v1",
+//     type: "object",
+//     required: ["title", "imageURL", "url", "author", "tags", "objectID"],
+//     //the schema itself
+//     properties: {
+//       title: { type: "string" },
+//       imageURL: { type: "string" },
+//       url: { type: "string" },
+//       author: { type: "string" },
+//       tags: { type: "number" },
+//       object: { type: "number" },
+//     },
+//   }
+  
 
 Post.plugin(beautifyUnique)
 
