218e465f7c5e392a2b218c57104a9eb5cd2164d3
seungguini <sl6369@nyu.edu>
2021-11-21 14:00:09 -0500

    Define Restaurant schema

--- back-end/models/restaurantModel.js
+++ back-end/models/restaurantModel.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create exerciseSchema - we define the schema before compiling it into a model
+// Data keys are a cleaned result from the Yelp API response (https://www.yelp.com/developers/documentation/v3/business_search)
 const restaurantSchema = new Schema(
   {
     // Add the username field to the exerciseSchema schema
@@ -15,31 +16,64 @@ const restaurantSchema = new Schema(
       type: String,
       required: true,
     },
+    yelp_id: {
+      type: String,
+      required: true
+    },
+    yelp_url : {
+      type: String,
+      required: true,
+    },
+    categories: {
+      alias: {
+        type: String,
+        required: true
+      }
+    },
+    restaurant_img_url: { // URL
+      type: String,
+      required: true,
+    },
+    location : {
+      coordinates: {
+        longitude: {
+          type: Number,
+          required: true,
+        },
+        latitude: {
+          type: Number,
+          required: true,
+        },
+      },
+      city: {
+        type: String,
+        required: true
+      },
+      country: {
+        type: String,
+        required: true
+      },
+      address1: {
+        type: String,
+        required: true
+      },
+      zip_code: {
+        type: String,
+        required: true
+      },
+    },
+    transactions: { // pickup, delivery
+      type: String,
+      required: true
+    },
     no_posts: {
       type: Number,
       required: true,
     },
-    backgroundPic: { // URL
-      type: String,
-      required: true,
-    },
-    location: {
-      longitude: {
-        type: Number,
-        required: true,
-      },
-      latitude: {
-        type: Number,
-        required: true,
-      },
-    },
-    menuPopular: {
 
-    }
+    posts: [  { type: Schema.Types.ObjectId, ref: 'Post' }]
   }
 );
-
-const 
 
 // Create a model called Exercise, compiled using the exerciseSchema - each instance of a Model is a document
 const Exercise = mongoose.model("Exercise", exerciseSchema);
