2b302b7eb9e485ff41a901f03d95e7a61c783f45
seungguini <sl6369@nyu.edu>
2021-11-21 17:29:41 -0500

    Add test scripts for instantiating Restaurant Schema

--- back-end/models/restaurantModel.js
+++ back-end/models/restaurantModel.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
 const URLSlugs = require('mongoose-url-slugs');
 
-const Schema = mongoose.Schema;
-
 // Create exerciseSchema - we define the schema before compiling it into a model
 // Data keys are a cleaned result from the Yelp API response (https://www.yelp.com/developers/documentation/v3/business_search)
 const Restaurant = new Schema(
@@ -12,76 +10,70 @@ const Restaurant = new Schema(
     // Add the username field to the exerciseSchema schema
     name: {
       type: String,
-      required: true,
     },
     phone_number: {
       type: String,
-      required: true
     },
     address: {
       type: String,
-      required: true,
     },
     yelp_id: {
       type: String,
-      required: true,
-      unique: true // To avoid duplicate restaurants
     },
     yelp_url : {
       type: String,
-      required: true,
     },
     categories: {
       alias: {
         type: String,
-        required: true
-      }
+      },
+      title: {
+        type: String,
+      },
     },
+
     restaurant_img_url: { // URL
       type: String,
-      required: true,
     },
+
     location : {
       coordinates: {
         longitude: {
           type: Number,
-          required: true,
         },
         latitude: {
           type: Number,
-          required: true,
         },
       },
       city: {
         type: String,
-        required: true
       },
       country: {
         type: String,
-        required: true
+      },
+      state: {
+        type: String,
       },
       address1: {
         type: String,
-        required: true
       },
       zip_code: {
         type: String,
-        required: true
       },
     },
-    transactions: { // pickup, delivery
-      type: String,
-      required: true
-    },
 
-    posts: [  { type: Schema.Types.ObjectId, ref: 'Post' }]
-  }
-);
+    transactions: [{ // pickup, delivery
+      type: String,
+    }],
+
+    posts: [{ type:mongoose.Schema.Types.ObjectId
+      , ref: 'Post' }]
+  });
 
 
 Restaurant.plugin(beautifyUnique)
 
 // Create a model called Exercise, compiled using the exerciseSchema - each instance of a Model is a document
-const Restaurant = mongoose.model("Restaurant", Restaurant);
+const RestaurantModel = mongoose.model("Restaurant", Restaurant);
 
-module.exports = Restaurant;
+module.exports = RestaurantModel;
