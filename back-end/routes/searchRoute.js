3d43d404265d169170162183d9dafd789e282116
Christian B Weinschenk <cbw307@nyu.edu>
2021-11-21 22:39:24 -0500

    completed Post schema as well as first version of search retrieval from database
    In this the search retrieval from post collection in database is untested (though it doesnt break the rest of the server) pending the add post to collection entry in the database. Populates will likely be tweaked in the future.

--- back-end/routes/searchRoute.js
+++ back-end/routes/searchRoute.js
@@ -2,14 +2,19 @@
 
 const router = require("express").Router(); // Router object to define a route
 const mockaroo = require("mockaroo")
+const mongoose = require("mongoose")
+const db = require('../models/postModel')
+const Post = db.postStuff;
 
 let client = new mockaroo.Client({
     apiKey: "329a3210"
 })
 
 
+
 const getSearch = async (req, res) =>{
     try{
+        
         // console.log("h1")
         //  const mockData = await fetch("https://my.api.mockaroo.com/post.json?key=329a3210").then(console.log("hji"))
         // console.log(mockData);
@@ -17,12 +22,25 @@ const getSearch = async (req, res) =>{
         // res.json(mockData);
         // res.send("hi")
         //generates fake data using mockaroo and sends out json response when this route is called
-        client.generate({
-            count: 10,
-            schema: "post"
-        }).then(function(records){
-            res.json(records)
-        })
+        // client.generate({
+        //     count: 10,
+        //     schema: "post"
+        // }).then(function(records){
+        //     res.json(records)
+        // })
+        // Post.Find({}, function(err, posts){
+        //     var postMap= {}
+
+        //     posts.foreach(function(post){
+        //         userMap[user._id] = user
+        //     })
+        //     res.json(postMap)
+
+        // })
+
+        const post = await Post.find({}).populate("author").populate("parentRestaurant")
+
+        
     }catch(error){res.send(error)}
 }
 
@@ -30,5 +48,6 @@ const getSearch = async (req, res) =>{
 router.route("/").get((req, res) => {
     getSearch(req, res);
 });
+
 
 module.exports = router
