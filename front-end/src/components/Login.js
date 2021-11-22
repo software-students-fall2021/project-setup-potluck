7d4fefda29d7829f35f7f88048d5e2a23097fe9e
Lauren Gatesman <55667325+lkgatesman@users.noreply.github.com>
2021-11-21 21:59:49 -0500

    Fixed login.js

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { Navigate } from "react-router-dom"
+import { Route } from "react-router-dom"
 import axios from "axios"
 //import "./Login.css"
 
@@ -71,7 +71,7 @@ const Login = props => {
     )
   // otherwise, if the user has successfully logged-in, redirect them to a different page
   // in this example, we simply redirect to the home page, but a real app would redirect to a page that shows content only available to logged-in users
-  else return <Navigate to="/" />
+  else return <Route to="/" />
 }
 
 export default Login
