9fbc5bb59e970accfcd2e5ecb1591741ab57a81c
Wajahat Mirza <mwm356@nyu.edu>
2021-11-22 15:23:28 -0500

    backend route for feed

---
+++ back-end/routes/postFeed.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = require("express").Router(); // Router object to define a route
+const mockaroo = require("mockaroo")
+const axios = require("axios")
+const postfeed = async (req, res) => {
+    try {
+        console.log("AXIOS RESULTS")
+        const response = await axios('https://api.mockaroo.com/api/6876d7a0?count=1000&key=f9dbb980')
+        const data = response.data // Extract data from mockaroo
+
+        res.send(data)
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+router.route("/").post((req, res) => {
+    postfeed(req, res);
+});
+console.log("fine here")
+module.exports = postfeed;
